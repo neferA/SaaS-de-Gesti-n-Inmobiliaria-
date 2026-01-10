22566172d78672b8958901833083a318f7e48486
ogi3r97 <wjaguilar-es@udabol.edu.bo>
2026-01-09 21:18:51 -0400

    initial_components_shadcn

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './modules/core/styles/index.css'
+import App from './modules/core/components/app'
 
 createRoot(document.getElementById('root')!).render(
     <App />
---
+++ src/modules/core/components/app.tsx
@@ -0,0 +1,8 @@
+
+export default function App() {
+    return(
+        <div className="p-4">
+            <p>hola mundo</p>        
+        </div>
+    );
+}
